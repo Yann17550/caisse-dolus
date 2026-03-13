05cf6d2c7ac659f19a2987f2d2fb99d0004a68cc
Yann17550 <millebulle@gmail.com>
2026-03-13 21:42:09 +0100

    Update logic-recap.js

--- logic-recap.js
+++ logic-recap.js
@@ -7,33 +7,39 @@ const LogicRecap = {
         const cbAmex = getV('total-amex');
         const cbTotal = cbClassic + cbAmex;
 
-        const results = {
-            especesReel: getV('total-cash-net'),
-            especesLogiciel: getIn('pos-cash'),
-            cbTotal: cbTotal,
-            cbTR: getV('total-tr'),
-            cbAncv: getV('total-ancv-connect'),
-            ancvPapier: getV('total-ancv-paper'),
-            cheques: getV('total-checks'),
-            mypos: getV('total-mypos'),
-            tva5: getIn('tva-5'),
-            tva10: getIn('tva-10'),
-            tva20: getIn('tva-20')
+        // On crée une structure plate et claire
+        const calc = {
+            details: {
+                especesReel: getV('total-cash-net'),
+                especesLogiciel: getIn('pos-cash'),
+                cbTotal: cbTotal,
+                cbTR: getV('total-tr'),
+                ancvConnect: getV('total-ancv-connect'), // CB-ANCV
+                ancvPapier: getV('total-ancv-paper'),
+                checks: getV('total-checks'),
+                mypos: getV('total-mypos'),
+                tva5: getIn('tva-5'),
+                tva10: getIn('tva-10'),
+                tva20: getIn('tva-20'),
+                totalTTC_TVA: 0,
+                deltaEspeces: 0
+            },
+            sommeGlobaleLogiciel: 0
         };
 
-        // Delta Espèces
-        results.deltaEspeces = results.especesReel - results.especesLogiciel;
+        // Calculs dérivés
+        calc.details.deltaEspeces = calc.details.especesReel - calc.details.especesLogiciel;
+        calc.details.totalTTC_TVA = calc.details.tva5 + calc.details.tva10 + calc.details.tva20;
 
-        // Somme Globale Logiciel (Demandée spécifiquement)
-        results.sommeGlobale = results.especesLogiciel + 
-                               results.cbTotal + 
-                               results.cbTR + 
-                               results.cbAncv + 
-                               results.ancvPapier + 
-                               results.cheques;
+        // Somme Globale (Espèces Logiciel + CB + CB-TR + CB-ANCV + ANCV Papier + Chèques)
+        calc.sommeGlobaleLogiciel = 
+            calc.details.especesLogiciel + 
+            calc.details.cbTotal + 
+            calc.details.cbTR + 
+            calc.details.ancvConnect + 
+            calc.details.ancvPapier + 
+            calc.details.checks;
 
-        results.totalTVA = results.tva5 + results.tva10 + results.tva20;
-
-        return results;
+        return calc;
     }
 };
